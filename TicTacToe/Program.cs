74ead7343370115ffdafe05b401e5e69e03f12bf
amilacic <aleksandar.milacic@gmail.com>
2023-07-09 19:07:30 +0200

    Add project files.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static char[,] board = new char[3, 3];
+        static char currentPlayer = 'X';
+
+        static void Main(string[] args)
+        {
+            InitializeBoard();
+            bool gameOver = false;
+
+            while (!gameOver)
+            {
+                DrawBoard();
+                Console.WriteLine($"Player {currentPlayer}'s turn. Enter the row (0-2):");
+                int row = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the column (0-2):");
+                int col = int.Parse(Console.ReadLine());
+
+                if (IsValidMove(row, col))
+                {
+                    MakeMove(row, col);
+                    if (IsGameOver())
+                    {
+                        DrawBoard();
+                        Console.WriteLine($"Player {currentPlayer} wins!");
+                        gameOver = true;
+                    }
+                    else if (IsBoardFull())
+                    {
+                        DrawBoard();
+                        Console.WriteLine("It's a draw!");
+                        gameOver = true;
+                    }
+                    else
+                    {
+                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move. Try again.");
+                }
+            }
+        }
+
+        static void InitializeBoard()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+        }
+
+        static void DrawBoard()
+        {
+            Console.WriteLine("---------");
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write($"| {board[row, col]} ");
+                }
+                Console.WriteLine("|");
+                Console.WriteLine("---------");
+            }
+        }
+
+        static bool IsValidMove(int row, int col)
+        {
+            if (row < 0 || row >= 3 || col < 0 || col >= 3)
+            {
+                return false;
+            }
+
+            return board[row, col] == ' ';
+        }
+
+        static void MakeMove(int row, int col)
+        {
+            board[row, col] = currentPlayer;
+        }
+
+        static bool IsGameOver()
+        {
+            // Check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] != ' ' && board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+                {
+                    return true;
+                }
+            }
+
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] != ' ' && board[0, col] == board[1, col] && board[1, col] == board[2, col])
+                {
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            if (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                return true;
+            }
+            if (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool IsBoardFull()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (board[row, col] == ' ')
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Tic-Tac-Toe
+
+The Tic-Tac-Toe game is a classic two-player game played on a 3x3 grid. This project provides a simple implementation of the Tic-Tac-Toe game in C#. It allows players to take turns, make moves, and determines the winner or a draw condition.
+
+## Getting Started
+
+To play the Tic-Tac-Toe game, follow these steps:
+
+1. Clone or download the project files to your local machine.
+
+2. Open the project in your preferred C# development environment (e.g., Visual Studio).
+
+3. Build the project to compile the source code.
+
+4. Run the application, and the game will start in the console window.
+
+5. Players will take turns entering their moves by providing the row and column numbers.
+
+6. The game will display the updated board after each move.
+
+7. If a player achieves a winning combination or the game ends in a draw, the game will announce the result.
+
+8. To start a new game, run the application again.
+
+## Screenshots
+
+Include one or more screenshots of the game in action to provide a visual representation of the gameplay.
+
+## Contributing
+
+Contributions to this project are welcome! If you find any issues or have suggestions for improvements, feel free to submit a pull request or open an issue.
+
+When contributing, please ensure that your code follows the established coding style and practices. Additionally, provide clear and concise commit messages to facilitate the review process.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+Mention any resources, tutorials, or inspiration that you used to develop this project.
+
+---
